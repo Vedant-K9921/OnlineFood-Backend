ac5a178f74e3a831262f3fb7df63c6e5faf0e7af
Vedant Kamble <vedantkamble9921@gmail.com>
2026-08-23 21:15:05 +0530

    fix: support all restaurants owned by an owner

--- src/main/java/com/localbites/repository/RestaurantRepository.java
+++ src/main/java/com/localbites/repository/RestaurantRepository.java
@@ -3,10 +3,12 @@ package com.localbites.repository;
 import com.localbites.entity.Restaurant;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
-public interface RestaurantRepository
-        extends JpaRepository<Restaurant, Long> {
+public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
 
     Optional<Restaurant> findByOwnerId(Long ownerId);
+
+    List<Restaurant> findAllByOwnerId(Long ownerId);
 }
