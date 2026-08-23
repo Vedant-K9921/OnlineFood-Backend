77406c47018dba4829293f65fc8b58faae585142
Vedant Kamble <vedantkamble9921@gmail.com>
2026-08-23 21:16:40 +0530

    security: restrict websocket origins

--- src/main/java/com/localbites/config/WebSocketConfig.java
+++ src/main/java/com/localbites/config/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package com.localbites.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -8,31 +9,21 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 
 @Configuration
 @EnableWebSocketMessageBroker
-public class WebSocketConfig
-        implements WebSocketMessageBrokerConfigurer {
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Value("${frontend.url}")
+    private String frontendUrl;
 
     @Override
-    public void configureMessageBroker(
-            MessageBrokerRegistry registry
-    ) {
-
-        registry.enableSimpleBroker(
-                "/topic"
-        );
-
-        registry.setApplicationDestinationPrefixes(
-                "/app"
-        );
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/app");
     }
 
     @Override
-    public void registerStompEndpoints(
-            StompEndpointRegistry registry
-    ) {
-
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-        .setAllowedOriginPatterns("*")
-        .withSockJS();
-
+                .setAllowedOrigins(frontendUrl)
+                .withSockJS();
     }
 }
